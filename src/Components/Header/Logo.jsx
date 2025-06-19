2ae1972c9bc4f3de35e01df5b2708ee2cba817a8
doaa <doaafakher0612@gmail.com>
2025-06-19 14:34:42 +0300

    edit logo path

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import logo from "../../assets/eCommerceLogo.jpg"
 export default function Logo() {
   return (
     <>
       <div id="logo-container" className="logo-container h-10 w-10 md:h-20 md:w-20">
         <a href="#header">
           <img
-            src="src\assets\eCommerce-logo.jpg"
+            src= {logo}
             className="h-full w-full cursor-pointer object-contain"
           />
         </a>
